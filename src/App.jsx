da109879fa4804febfe143a10dc4f5780c60894a
Joseph <joseph8778@gmail.com>
2025-01-25 15:26:21 -0600

    author page configured

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import ItemDetails from "./pages/ItemDetails";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 
+
 function App() {
   return (
     <Router>
@@ -13,12 +14,13 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/explore" element={<Explore />} />
-        <Route path="/author" element={<Author />} />
+        <Route path="/author/:authorId" element={<Author />} />
         <Route path="/item-details" element={<ItemDetails />} />
       </Routes>
       <Footer />
     </Router>
   );
 }
+
 
 export default App;
